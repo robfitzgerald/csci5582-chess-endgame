b26981d587196c783c6449181c595fb7a2fa6cd6
Robert Fitzgerald <robfitzgerald@gmail.com>
2015-09-14 20:47:24 -0600

    gitignore xcode

---
+++ CSCI5582-chess-endgame/CSCI5582-chess-endgame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSCI5582-chess-endgame
+//
+//  Created by Robert Fitzgerald on 9/14/15.
+//  Copyright (c) 2015 Rob Fitzgerald. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
